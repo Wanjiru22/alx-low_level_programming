67608366f404242152dfbae79e1b8b89a61af11d
Wanjiru22 <wanjirushaleen@gmail.com>
2022-09-11 05:56:40 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Prints lowercase letters except q and e
+* Return: Always 0
+*/
+int main(void)
+{
+	char lowerCase;
+	
+	for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
+	{
+		if (lowerCase != 'e' && lowerCase != 'q')
+			putchar(lowerCase);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
