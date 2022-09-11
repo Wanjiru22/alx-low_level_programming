b4b0cd2f6eff4d1c2ff04f9598683fc57150b570
Wanjiru22 <wanjirushaleen@gmail.com>
2022-09-10 22:34:55 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,7 +6,8 @@
 int main(void)
 {
 	char lowerCase;
-	for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase ++)
+
+	for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
 		putchar(lowerCase);
 	putchar('\n');
 
