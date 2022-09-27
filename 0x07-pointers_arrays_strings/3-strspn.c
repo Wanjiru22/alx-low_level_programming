0ff2e3edae17aa875a24f1ca41eb0dcb3258bd8e
Wanjiru22 <wanjirushaleen@gmail.com>
2022-09-27 06:22:57 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,13 +10,13 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
+	unsigned int i; j;
 
 	for (i = 0; s[i]; i++)
 	{
 		for (j = 0; accept[j]; j++)
 		{
-			if (s[j] == accept[j])
+			if (s[i] == accept[j])
 				break;
 		}
 		if (!accept[j])
