4c2884cee508758dbd0e59c68c82e079b90ca6f7
Wanjiru22 <wanjirushaleen@gmail.com>
2022-10-05 19:58:26 -0700

    Task 6

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 
 int _putchar(char);
+int len(char *str);
+int num_words(char *str);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
