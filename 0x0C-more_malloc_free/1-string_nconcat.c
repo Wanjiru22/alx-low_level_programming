bcf1191206c67ca6ec55c4ba0ab1815ca152cf58
Wanjiru22 <wanjirushaleen@gmail.com>
2022-10-06 15:41:56 -0700

    Task 0

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	if (j > n)
 		j = n;
-	s = malloc(sizeof(char) * (i + j + l));
+	s = malloc(sizeof(char) * (i + j + k));
 	if (s == NULL)
 		return (NULL);
 	for (k = 0; k < i; k++)
